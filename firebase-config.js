bcf5cf46e5c315578e15ddbfa24239f9701ce13c
bethbenettt-afk <bethbenettt-afk@users.noreply.github.com>
2026-07-14 15:40:23 -0700

    Enable Firebase sync and clean deployment files

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCq9f_DifixwUvXtlKmlGauNHQlLsIl5RI",
+  authDomain: "contra-tenebras.firebaseapp.com",
+  projectId: "contra-tenebras",
+  storageBucket: "contra-tenebras.firebasestorage.app",
+  messagingSenderId: "184151211779",
+  appId: "1:184151211779:web:a5c0eabc6ac78110cbb336",
+  measurementId: "G-65LFKB7H9Y"
 };
